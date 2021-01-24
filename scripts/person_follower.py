fcf5fd3553c8ee568bd3afa7d83eb0b2443a2f89
Leandra Nealer <lnealer@kingler.cs.uchicago.edu>
2021-01-23 23:19:23 -0600

    adding comments

--- scripts/person_follower.py
+++ scripts/person_follower.py
@@ -15,19 +15,22 @@ class PersonFollow:
 		rospy.Subscriber('scan', LaserScan, self.process_scan)
 	def process_scan(self,data):
 		closest = float('inf')
-		angle = 0
+		angle = 0 # initializing variables
+		# iterate to find closest item to bot
 		for i in range(360):
 			if data.ranges[i] < closest:
 				closest = data.ranges[i]
 				angle = i
 		if (closest == float('inf')):
+		# if no one is within range, do nothing
 				closest = 0.5
 		if (angle > 180):
+			# converting angle to negative if right of 0 degrees
 			angle = angle - 360
-		error_a = angle * 0.01745329
+		error_a = angle * 0.01745329 #convert to radians, error is difference from 0
 		error_d = closest - 0.5
-		k_d = 0.55
-		k_a = 2.3
+		k_d = 0.55 # k for distance
+		k_a = 2.3 # k for angle
 		self.vel_msg.linear.x = k_d * error_d
 		self.vel_msg.angular.z = k_a * error_a
 		self.vel_pub.publish(self.vel_msg)
--- scripts/wall_follower.py
+++ scripts/wall_follower.py
@@ -15,16 +15,19 @@ class WallFollow:
 		self.vel_pub.publish(self.vel_msg)
 		rospy.Subscriber('scan', LaserScan, self.process_scan)
 	def process_scan(self,data):
-		forward_dist = data.ranges[0]
-		right_dist = data.ranges[270]
+		forward_dist = data.ranges[0] #distance in front of bot
+		right_dist = data.ranges[270] #distance to right of bot
 		if (forward_dist<0.2):
+			# bot stuck on wall
 			self.vel_msg.linear.x = -0.3
 			self.vel_pub.publish(self.vel_msg)
 		elif (forward_dist <= 0.9):
+			# make a right turn
 			self.vel_pub.publish(self.vel_msg)
 			self.vel_msg.angular.z = 1.5707963267948966192313216916397514420985846996875529104874722961
 			self.vel_pub.publish(self.vel_msg)
 		elif (forward_dist > 0.9):
+			#adjust to stay within 0.5 of wall
 			self.vel_msg.linear.x = 0.4
 			self.vel_pub.publish(self.vel_msg)
 			k = 0.1
